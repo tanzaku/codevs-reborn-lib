f636e028e2258faa11a869e5b0b9329c1c4ad89b
user <ume.drink@gmail.com>
2019-05-18 18:30:57 +0900

    fix

--- src/rensa_plan.rs
+++ src/rensa_plan.rs
@@ -108,7 +108,7 @@ pub fn calc_rensa_plan<F>(context: &PlanContext, rand: &mut rand::XorShiftL, cal
     let initial_state = BeamState::new(context.player.clone(), 0, 0);
     heaps[0].push(initial_state);
 
-    // let mut visited = vec![HashSet::new(); context.max_turn];
+    let mut visited = HashSet::new();
 
     let board_is_empty = context.player.board.is_empty();
     let mut _iter = 0;
@@ -143,86 +143,7 @@ pub fn calc_rensa_plan<F>(context: &PlanContext, rand: &mut rand::XorShiftL, cal
                     let actions = push_action(b.actions, a);
                     
                     // if player.board.is_dead() || !context.enemy_send_obstacles.is_empty() && !visited.insert(player.hash()) {
-                    // if player.board.is_dead() || !visited[search_turn].insert(player.hash()) {
-                    if player.board.is_dead() {
-                        return;
-                    }
-                    let score = score * 256 + (rand.next() & 0xFF) as i64;
-                    let eval_score = eval_score * 256 + (rand.next() & 0xFF) as i64;
-                    if search_turn + 1 < context.max_turn {
-                        heaps[search_turn + 1].push(BeamState::new(player.clone(), eval_score, actions));
-                    }
-                    if bests[search_turn].score < score {
-                        bests[search_turn] = SearchResult { score, actions, };
-                    }
-                });
-            };
-        });
-    }
-
-    // eprintln!("iter={}", _iter);
-    // bests.iter().for_each(|b| { eprintln!("obstacle={}", b.0.score / 10000000000); });
-    bests.into_iter().map(|b| {
-        let mut replay = replay::Replay::new();
-        let actions = b.get_actions();
-        let start_turn = context.plan_start_turn;
-        let last_turn = start_turn + actions.len();
-        replay.init(&context.player, &context.packs[start_turn..last_turn], context.enemy_send_obstacles, &actions);
-        replay
-    }).collect()
-}
-
-
-// ビームサーチ
-pub fn calc_rensa_plan2<F>(context: &PlanContext, rand: &mut rand::XorShiftL, calc_score: F) -> Vec<replay::Replay>
-    where F: Fn(&action::ActionResult, &player::Player, &board::Feature) -> i64 + Sync + Send
-{
-    assert!(context.max_turn <= 16);
-    let timer = Instant::now();
-
-    let actions = action::Action::all_actions();
-    let mut heaps = vec![BinaryHeap::new(); context.max_turn];
-
-    let mut bests: Vec<SearchResult> = vec![Default::default(); context.max_turn];
-    let initial_state = BeamState::new(context.player.clone(), 0, 0);
-    heaps[0].push(initial_state);
-
-    let mut visited = vec![HashSet::new(); context.max_turn];
-
-    let board_is_empty = context.player.board.is_empty();
-    let mut _iter = 0;
-    loop {
-        let elapsed = timer.elapsed();
-        let milli = elapsed.as_secs() * 1000 + (elapsed.subsec_nanos() / 1000_000) as u64;
-        if milli >= context.think_time_in_milli || heaps.iter().all(|h| h.is_empty()) {
-            break;
-        }
-
-        _iter += 1;
-
-        (0..context.max_turn).for_each(|search_turn| {
-            let turn = context.plan_start_turn + search_turn;
-
-            if let Some(b) = heaps[search_turn].pop() {
-                actions.iter().for_each(|a| {
-                    if &action::Action::UseSkill == a && !b.player.can_use_skill() {
-                        return;
-                    }
-
-                    if board_is_empty && turn == context.plan_start_turn {
-                        if let action::Action::PutBlock { pos, rot: _ } = a {
-                            if *pos != W / 2 {
-                                return;
-                            }
-                        }
-                    }
-
-                    let mut player = b.player.clone();
-                    let (score, eval_score) = do_action(&mut player, search_turn, context, a, &calc_score);
-                    let actions = push_action(b.actions, a);
-                    
-                    // if player.board.is_dead() || !context.enemy_send_obstacles.is_empty() && !visited.insert(player.hash()) {
-                    if player.board.is_dead() || !visited[search_turn].insert(player.hash()) {
+                    if player.board.is_dead() || !visited.insert(player.hash()) {
                     // if player.board.is_dead() {
                         return;
                     }
